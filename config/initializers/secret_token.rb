db54e7618baaf9800bb8b8b984f3950ce42374a9
Dan Cartoon <dcartoon@gmail.oom>
2012-02-11 12:14:59 -0800

    Adding initial Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logparty::Application.config.secret_token = '0b0b9a29238019dae9e443e119b82a84120a55d6a3f187e2210374949fd78bf34ca47fb1a7e1d473dce4ac5889ec4901b6edf63cc435c9f1dec36e599345afed'
